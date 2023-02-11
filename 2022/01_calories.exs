15b79dddb3513946af8cd711cdab1a425232694c
Andreas Duerloo <andreasduerloo@gmail.com>
2023-02-11 13:37:47 +0100

    2022 day 01

---
+++ 2022/01_calories.exs
@@ -0,0 +1,13 @@
+{_result, content} = File.read("./calories.txt")
+
+values = content
+  |> String.split("\n\n") # A list of strings
+  |> Enum.map(&(String.split(&1, "\n"))) # A list of lists of strings
+  |> Enum.map(&(Enum.map(&1, fn(s) -> String.to_integer(s) end))) # A list of lists of integers
+  |> Enum.map(&(Enum.sum(&1))) # A list of integers
+
+# First star
+values |> Enum.max() |> IO.puts()
+
+# Second star
+values |> Enum.sort(:desc) |> Enum.take(3) |> Enum.sum() |> IO.puts()
