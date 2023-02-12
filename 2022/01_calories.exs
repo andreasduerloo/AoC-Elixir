a16d682b4c38cd125bf323f6e9c510199911073d
Andreas Duerloo <andreasduerloo@gmail.com>
2023-02-12 11:31:46 +0100

    Clean up comments

--- 2022/01_calories.exs
+++ 2022/01_calories.exs
@@ -2,16 +2,9 @@
 
 values = content
   |> String.split("\n\n") # A list of strings
-  |> Enum.map(&(String.split(&1, "\n"))) # A list of lists of strings
+  |> Enum.map(&(String.split(&1, "\n"))) # A list of lists of strings - Replace with Enum.chunk_by()? That gives issues with empty strings
   |> Enum.map(&(Enum.map(&1, fn(s) -> String.to_integer(s) end))) # A list of lists of integers
   |> Enum.map(&(Enum.sum(&1))) # A list of integers
-
-# Replace with Enum.chunk_by()?
-# values = content
-#   |> String.split("\n")
-#   |> Enum.chunk_by(&(&1 != "")) # Issue with empty strings
-#   |> Enum.map(&(Enum.map(&1, fn(s) -> String.to_integer(s) end)))
-#   |> Enum.map(&(Enum.sum(&1))) # A list of integers
 
 # First star
 values |> Enum.max() |> IO.puts()
